0826dec2207af0a61da1ef869a8e5daf268204da
Robert Weller <rob31989@yahoo.com>
2018-08-22 15:11:15 -0400

    removal of js hint since it wasn't playing nice with react

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  firstName: 'Rob',
+  lastName: 'Weller'
+};
+
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
+
+function functionalComponent(props){
+  return <div>This is just to test out if this works. {props.firstName}</div>
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />          
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React, {formatName(user)}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {functionalComponent(user)}
       </div>
     );
   }
